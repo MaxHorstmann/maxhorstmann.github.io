594df78382d4c2b75bcf05a751c1f64cb5fea609
Max Horstmann <max.horstmann@gmail.com>
2014-12-28 16:57:47 -0500

    force polling

--- vagrant.sh
+++ vagrant.sh
@@ -4,5 +4,5 @@ apt-get update
 apt-get -y install build-essential git ruby1.9.3 nodejs
 gem install github-pages --no-ri --no-rdoc
 cd /vagrant
-jekyll serve --watch --drafts
+jekyll serve --watch --force_polling --drafts
 
