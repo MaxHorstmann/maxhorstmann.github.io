a2015c92a6b12b93cf9f3004a715c4643e54eeba
Max Horstmann <max.horstmann@gmail.com>
2015-03-14 13:54:57 -0400

    fix vagrant

--- vagrant.sh
+++ vagrant.sh
@@ -2,6 +2,7 @@
 
 apt-get update 
 apt-get -y install build-essential git ruby1.9.3 nodejs
+gem install ffi -v 1.9.3
 gem install github-pages --no-ri --no-rdoc
 cd /vagrant
 jekyll serve --watch --force_polling --drafts
