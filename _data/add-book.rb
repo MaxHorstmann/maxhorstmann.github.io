324c70675e3d57605f604bcf125a4a804ade362e
Max Horstmann <max.horstmann@gmail.com>
2015-03-14 19:53:22 -0400

    ruby ruby

--- _data/add-book.rb
+++ _data/add-book.rb
@@ -1,6 +1,19 @@
-require 'net/http'
-require 'net/https'
+require 'open-uri'
+require 'json'
+
+isbn = ARGV[0]
+
+if (isbn.to_s == '') 
+    puts "usage: ruby findbook.rb <isbn>" 
+else
+    url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn
+    json = open(url, 'Accept-Encoding' => '').read
+    volumeInfo = JSON.parse(json)["items"][0]["volumeInfo"];
+    title = volumeInfo["title"] 
+    subtitle = volumeInfo["subtitle"] 
+    authors = volumeInfo["authors"].join()
+    thumbnail = volumeInfo["imageLinks"]["smallThumbnail"] 
+    puts "\"#{title}\";\"#{subtitle}\";\"#{authors}\";\"#{thumbnail}\""
+end
 
 
-json = Net::HTTP.get(URI.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:9780984999309'))
-puts json
