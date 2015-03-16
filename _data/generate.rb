5778af576524313b7fa7a1e14cddd2bb7f1397c2
Max Horstmann <max.horstmann@gmail.com>
2015-03-15 20:10:20 -0400

    moar books

--- _data/generate.rb
+++ _data/generate.rb
@@ -6,7 +6,8 @@ out = []
 CSV.foreach("isbn.csv") do |row|
     url = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + row[0]
     book = JSON.parse(open(url, 'Accept-Encoding' => '').read)
-    book["tldr"] = row[1]
+    book["group"] = row[1]
+    book["tldr"] = row[2]
     out.push(book)
 end
 puts out.to_json
