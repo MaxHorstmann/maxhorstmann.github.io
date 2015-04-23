23e99787d4c673fe40f2d2307125ceb6c3fd974f
Max Horstmann <max.horstmann@gmail.com>
2015-04-22 21:06:33 -0400

    moar angular

--- stacksocial.js
+++ stacksocial.js
@@ -1,19 +1,41 @@
-SE.init({
-    clientId: 4686,
-    key: 'Jn1HoRLSkS1IMtHxX0Tw0A((',
-    channelUrl: 'http://maxhorstmann.net/blank',
-    complete: function (data) { 
-        //alert("complete"); 
-    }
-});
+//var app = angular.module('app', ['firebase']);
+var app = angular.module('app', []);
 
-function login() {
-    SE.authenticate({
-        success: function(data) { 
-            alert('success'); 
-            console.log(data);
-        },
-        error: function(data) { alert('error'); },
-        networkUsers: true });
+// app.factory('firebaseConnection', ['$firebase', function($firebase) {
+//     var firebase_url = document.getElementById('data-firebase-url').getAttribute('data-firebase-url');
+//     var firebaseRef = new Firebase(firebase_url);
+//     return { 
+//       firebase_url : firebase_url, 
+//       firebaseRef : firebaseRef,
+//       user : {}
+//     };
+// }]);
 
-}    
+//app.controller('controller', ['$scope', 'firebaseConnection', function($scope, firebaseConnection) {
+app.controller('controller', ['$scope', function($scope) {
+
+    $scope.login = function() {
+        SE.authenticate({
+            success: function(data) { 
+                alert('success'); 
+                console.log(data);
+            },
+            error: function(data) { alert('error'); },
+            networkUsers: true });
+    };
+    
+    $scope.initializing = true;
+
+    SE.init({
+        clientId: 4686,
+        key: 'Jn1HoRLSkS1IMtHxX0Tw0A((',
+        channelUrl: 'http://maxhorstmann.net/blank',
+        complete: function (data) { 
+            $scope.initializing = false;
+        }
+    });
+
+
+}]);
+
+
