aa558d5e42cc7b2a0b88d181e348339a0e70ab07
Max Horstmann <max.horstmann@gmail.com>
2015-04-22 20:50:34 -0400

    try it

---
+++ stacksocial.js
@@ -0,0 +1,14 @@
+SE.init({
+    clientId: 4686,
+    key: 'Jn1HoRLSkS1IMtHxX0Tw0A((',
+    channelUrl: 'http://maxhorstmann.net/blank',
+    complete: function (data) { alert("complete"); }
+});
+
+function login() {
+    SE.authenticate({
+        success: function(data) { alert('success'); },
+        error: function(data) { alert('error'); },
+        networkUsers: true
+
+}    
