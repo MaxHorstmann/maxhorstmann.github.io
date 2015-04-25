f8de26de5adf534347c13c513682cd9bf33d4c0e
Max Horstmann <max.horstmann@gmail.com>
2015-04-24 20:18:27 -0400

    cookie

--- stacktrack.js
+++ stacktrack.js
@@ -5,15 +5,15 @@ app.controller('controller', ['$scope', '$http', '$cookies', function($scope, $h
     $scope.apiRoot = 'https://api.stackexchange.com/2.2';
     $scope.key = 'Jn1HoRLSkS1IMtHxX0Tw0A((';
 
+
+
     $scope.login = function() {
         SE.authenticate({
             success: function(data) { 
                 $scope.auth = data;
                 $scope.$apply();
 
-                $cookies['accessToken'] = data.accessToken;
-
-
+                $cookies.accessToken = data.accessToken;
                 $scope.whoami();
             },
             error: function(data) { alert('error'); },
@@ -23,7 +23,7 @@ app.controller('controller', ['$scope', '$http', '$cookies', function($scope, $h
 
     $scope.logout = function() {
         $scope.auth = null;
-        $cookies['accessToken'] = '';
+        $cookies.accessToken = '';
         $scope.user = null;
         $scope.following = null;
         $scope.timeline = null;
@@ -40,16 +40,26 @@ app.controller('controller', ['$scope', '$http', '$cookies', function($scope, $h
                 $scope.initializing = false;
                 $scope.$apply();
 
-                var accessToken = $cookies["accessToken"];
+                var accessToken = $cookies.accessToken;
                 if ((accessToken) && (len(accessToken)>0)) {
                     $scope.auth = {
                         'accessToken' : accessToken
                     };
                     $scope.whoami();
+
+                    var following = $cookies.following;
+                    if (following) {
+                        $scope.following = following;
+                    }
                 };
 
             }
         });
+
+    $scope.followNewUser = function() {
+        $scope.following.push($scope.followNew);
+        $scope.followNew = '';
+    }
 
 
 
@@ -67,7 +77,6 @@ app.controller('controller', ['$scope', '$http', '$cookies', function($scope, $h
             var timelineUrl = $scope.apiRoot + "/users/22656/timeline?site=stackoverflow&key=" + $scope.key +"&access_token=" + $scope.auth.accessToken;
             $http.get(timelineUrl).
               success(function(data, status, headers, config) {
-                console.log(data);
                 $scope.timeline = data.items;
               });
 
